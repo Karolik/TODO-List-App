05b8cdf05de0c144cacdc504c7916b69b873c39f
Karolina Gauvain <karolina.gauvain@gmail.com>
2019-06-06 18:35:31 +0200

    Add comments

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -7,18 +7,20 @@ $("ul").on("click", "li", function(){
 $("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
-	});
-	event.stopPropagation();
+  }); //before removes the "li", there is the effect of fading out
+  event.stopPropagation(); //stops event bubbling (the parent "li" doesn't get fired when we click the span)
 });
 
-$("input[type='text']").keypress(function(event){
-	if(event.which === 13){
-		//grabbing new todo text from input
-		var todoText = $(this).val();
-		$(this).val("");
-		//create a new li and add to ul
+$("input[type = 'text']").keypress(function(event){
+  if(event.which === 13){ //Check for Enter 
+    //Grab new todo text from input:
+    let todoText = $(this).val();
+    //Create a new li and add to ul
 		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
-	}
+    //Clear the input
+    $(this).val("");
+    console.log($(this).val()); //Print the value of the input
+  }
 });
 
 $("#toggle-form").click(function(){
